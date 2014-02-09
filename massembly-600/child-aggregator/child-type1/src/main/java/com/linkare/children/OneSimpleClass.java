38c2818549a85890c9417d605b465265e784d061
Karl Heinz Marbaise <info@soebes.de>
2014-02-09 23:48:20 +0100

    - Added examples project from MASSEMBLY-600

---
+++ massembly-600/child-aggregator/child-type1/src/main/java/com/linkare/children/OneSimpleClass.java
@@ -0,0 +1,12 @@
+package com.linkare.children;
+
+public class OneSimpleClass {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	System.out.println("Hello Assemblies!");
+    }
+
+}
